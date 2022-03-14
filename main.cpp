4598c9ad6f81c1784cf71ae803c8fa72409a0a25
RazvanDanciu28 <mariorazvan.2002@gmail.com>
2022-03-14 02:45:28 +0200

    14.03 02:45

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <queue>
 #include <chrono>
+#include <algorithm>
 using namespace std;
 using namespace std::chrono;
 ifstream fin("input.txt");
@@ -18,19 +19,27 @@ int test_sort (vector<int> v_sortat, int n)
 //functie care atriubuie valori random vectorului in functie de numarul de numere si numarul maxim
 void get_random_vector (vector<int> &v, int n, int maxx)
 {
+
+    auto start = high_resolution_clock::now();
+
     srand(time(0));
     for (int i = 0; i < n; i++){
         int rand_nr = (rand() * rand()) % (maxx + 1);
         v.push_back(rand_nr);
     }
 
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout<<"Numerele au fost randomizate in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
+
 }
 
 //Fiind o functie recursiva, nu pot masura timpul in cadrul functiei pentru ca se va autoapela si voi avea returnati mai multi timpi
 //asa ca voi masura timpul in main
-void MergeSort(vector<int> v, int st, int dr)
+void MergeSort(vector<int> &v, int st, int dr)
 {
 
+    ///maxim sir de 10^5 numere!!!!!
     //creez vector auxiliar pentru interclasare
     vector<int> w;
     if(st < dr)
@@ -61,12 +70,22 @@ void SelectionSort (vector<int> v, int n)
         for (int j = i + 1; j < n; j++){
             if (v[i] > v[j]) swap(v[i], v[j]);
         }
+
+        //la fiecare pas verific daca timpul de rulare sare de 10 minute
+        //daca timpul de rulare a sarit de 7 minute, opresc sortarea si afisez mesajul corespunzator
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        if (duration.count()/1000000.00000000000 > 420){
+            cout<<"Timpul de rulare pentru SelectionSort este prea mare, s-a oprit sortarea."<<endl;
+            return;
+
+        }
     }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     if (test_sort(v, n))
-        cout<<"SelectionSort a sortat corect numerele in "<<duration.count() <<" secunde"<< endl;
+        cout<<"SelectionSort a sortat corect numerele in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
     else
         cout<<"SelectionSort nu a sortat numerele corect"<<endl;
 }
@@ -74,11 +93,12 @@ void SelectionSort (vector<int> v, int n)
 void CountingSort(vector<int> v, int n, int maxx)
 {
     auto start = high_resolution_clock::now();
+
     int size = v.size();
     vector<int> w;
-    for (int i = 0 ; i <= maxx; i++) w.push_back(0);
-    for (auto it: v) w[it]++;
-    v.clear();
+    for (int i = 0 ; i <= maxx; i++) w.push_back(0); //imi creez vectorul de frecventa initializat cu 0
+    for (auto it: v) w[it]++; //completez vectorul de frecventa in functie de vectorul dat
+    v.clear(); //golesc vectorul ca apoi sa adaug numerele sortate conform vectorului de frecventa
     for (int i = 0; i <= maxx; i++){
         if (w[i]){
             for (int j = 1; j <= w[i]; j++) v.push_back(i);
@@ -88,15 +108,15 @@ void CountingSort(vector<int> v, int n, int maxx)
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     if (test_sort(v, n))
-        cout<<"CountingSort a sortat corect numerele in "<<duration.count() <<" secunde"<< endl;
+        cout<<"CountingSort a sortat corect numerele in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
     else
         cout<<"CountingSort nu a sortat numerele corect"<<endl;
 
-    w.clear();
+    w.clear(); //sterg vectorul de frecventa
 }
 
 //RadixSort doar in baza 10
-int RadixSort(vector<int> v, int n){
+void RadixSort(vector<int> v, int n){
     auto start = high_resolution_clock::now();
 
     //caut maximul pentru a sti cate cifre are cel mai mare si pana la cat merg cu forul
@@ -120,18 +140,28 @@ int RadixSort(vector<int> v, int n){
             }
         }
         delete[] w;
+
+        //la fiecare pas verific daca timpul de rulare sare de 10 minute
+        //daca timpul de rulare a sarit de 5 minute, opresc sortarea si afisez mesajul corespunzator
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        if (duration.count()/1000000.00000000000 > 300){
+            cout<<"Timpul de rulare pentru SelectionSort este prea mare, s-a oprit sortarea."<<endl;
+            return;
+
+        }
     }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     if (test_sort(v, n))
-        cout<<"RadixSort a sortat corect numerele in "<<duration.count() <<" secunde"<< endl;
+        cout<<"RadixSort a sortat corect numerele in "<<duration.count()/1000000.00000000000<<" secunde"<< endl;
     else
         cout<<"RadixSort nu a sortat numerele corect"<<endl;
 
 }
 
-
+//functia de InsertionSort folosita pentru ShellSort cand ajung la gap = 1
 void InsersionSort (vector<int> &v, int n)
 {
     for (int i = 1; i < n; i++){
@@ -156,48 +186,86 @@ void ShellSort (vector<int> v, int n)
             if ((i + gap < n) and (v[i] > v[i+gap])) swap(v[i], v[i+gap]);
         }
 
+        //la fiecare pas verific daca timpul de rulare sare de 10 minute
+        //daca timpul de rulare a sarit de 5 minute, opresc sortarea si afisez mesajul corespunzator
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        if (duration.count()/1000000.00000000000 > 300){
+            cout<<"Timpul de rulare pentru ShellSort este prea mare, s-a oprit sortarea."<<endl;
+            return;
+
+        }
+
     }
-    if (gap == 1 and !test_sort(v, n)) InsersionSort(v, n);
+    if (gap == 1 and !test_sort(v, n)) InsersionSort(v, n); // daca ajung la gap = 1 si vectorul inca nu e sortat, intru in InserstionSort
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
     if (test_sort(v, n))
-        cout<<"ShellSort a sortat corect numerele in "<<duration.count() <<" secunde"<< endl;
+        cout<<"ShellSort a sortat corect numerele in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
     else
         cout<<"ShellSort nu a sortat numerele corect"<<endl;
 }
+
 
 int main() {
 
 
     int n, maxx, numar_teste;
     vector <int> v;
-    cin>>numar_teste;
+    fin>>numar_teste;
     for (int i = 1; i <= numar_teste; i++){
-        cin>>n>>maxx;
+        fin>>n>>maxx;
+        cout<<"n = "<<n<<" "<<"maxx = "<<maxx<<":"<<endl;
         get_random_vector(v, n, maxx);
 
-        cout<<n<<" "<<maxx<<":"<<endl;
-        SelectionSort(v, n);
+        //sortarea nativa
+        vector<int> copie;
+        for (auto it: v) copie.push_back(it);
+        auto start = high_resolution_clock::now();
+        sort(copie.begin(), copie.end());
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        if (test_sort(copie, n))
+            cout<<"Sortarea nativa a compilatorului a sortat corect numerele in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
+        else
+            cout<<"Sortarea nativa a compilatorului nu a sortat numerele corect"<<endl;
+        copie.clear();
+
+        //MergeSort
+        if (n > 100000)
+            cout<<"Sunt prea multe numere pentru a fi sortate folosind MergeSort. Va dura prea mult sau se va ajunge la Stack Overflow."<<endl;
+        else{
+            for (auto it: v) copie.push_back(it);
+            auto start = high_resolution_clock::now();
+            MergeSort(copie, 0 , n-1);
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            if (test_sort(copie, n))
+                cout<<"MergeSort a sortat corect numerele in "<<duration.count()/1000000.00000000000 <<" secunde"<< endl;
+            else
+                cout<<"MergeSort nu a sortat numerele corect"<<endl;
+            copie.clear();
+        }
+
+        if (n >= 1000000)
+            cout<<"Sunt prea multe numere pentru a fi sortate folosind SelectionSort. Va dura prea mult."<<endl;
+        else
+            SelectionSort(v, n);
+
         RadixSort(v, n);
         CountingSort(v, n, maxx+1);
-        ShellSort(v, n);
 
+        if (n >= 10000000)
+            cout<<"Sunt prea multe numere pentru a fi sortate folosind ShellSort. Va dura prea mult."<<endl;
+        else
+            ShellSort(v, n);
 
+        cout<<endl<<endl;
         v.clear();
 
     }
 
-
-//    int n, maxx, numar_teste;
-//    vector <int> v;
-//    cin>>n;
-//    for (int i = 1; i <= n; i++){
-//        int x; cin>>x;
-//        v.push_back(x);
-//    }
-//    InsersionSort(v, n);
-//    for (auto it: v) cout<<it<<" ";
     return 0;
 }
